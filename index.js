d0f80524e0e0bc4b5e9af54915617b9c37b6000d
dalavaiyuvaraj <114892153+dalavaiyuvaraj@users.noreply.github.com>
2023-10-12 16:40:17 -0400

    uploading Assignment 4 (#3)
    * creating Vpn using pulumi
    
    * adding subnets and .env file
    
    * uploadingAssignment-4

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ const VPC_CIDR_prefix = process.env.VPC_CIDR_prefix;
 const VPC_name = process.env.VPC_name;
 const subnet_public_name = process.env.subnet_public_name;
 const subnet_private_name = process.env.subnet_private_name;
+const public_route_table = process.env.public_route_table;
+const private_route_table = process.env.private_route_table;
+const public_subnetconnect = process.env.public_subnetconnect;
+const private_subnetconnect = process.env.private_subnetconnect;
+const internet_gateway = process.env.internet_gateway;
+const public_route = process.env.public_route;
+const public_route_cidr_des = process.env.public_route_cidr_des;
 
 // Create a VPC
 const main = new aws.ec2.Vpc(VPC_name, {
@@ -61,3 +68,57 @@ for (let i = 0; i < numPrivateSubnets; i++) {
     privateSubnets.push(subnet);
 }
 
+// Create route tables
+const publicRouteTable = new aws.ec2.RouteTable(public_route_table, {
+    vpcId: main.id,
+    tags: {
+        Name: public_route_table,
+    },
+});
+
+const privateRouteTable = new aws.ec2.RouteTable(private_route_table, {
+    vpcId: main.id,
+    tags: {
+        Name: private_route_table,
+    },
+});
+
+const publicRouteTableAssociations = [];
+const privateRouteTableAssociations = [];
+
+// Associate public subnets with the public route table
+for (let i = 0; i < numPublicSubnets; i++) {
+    let x = i+1;
+    const association = new aws.ec2.RouteTableAssociation(public_subnetconnect+x, {
+        subnetId: publicSubnets[i].id,
+        routeTableId: publicRouteTable.id,
+    });
+
+    publicRouteTableAssociations.push(association);
+}
+
+// Associate private subnets with the private route table
+for (let i = 0; i < numPrivateSubnets; i++) {
+    let x = i+1;
+    const association = new aws.ec2.RouteTableAssociation(private_subnetconnect+x, {
+        subnetId: privateSubnets[i].id,
+        routeTableId: privateRouteTable.id,
+    });
+
+    privateRouteTableAssociations.push(association);
+}
+
+// Create an Internet Gateway
+const internetGateway = new aws.ec2.InternetGateway(internet_gateway, {
+    vpcId: main.id,
+    tags: {
+        Name: internet_gateway,
+    },
+});
+
+// Create a public route in the public route table
+const publicRoute = new aws.ec2.Route(public_route, {
+    routeTableId: publicRouteTable.id,
+    destinationCidrBlock: public_route_cidr_des,
+    gatewayId: internetGateway.id,
+});
